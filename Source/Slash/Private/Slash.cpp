22f14f633b64e98f9e4828afb15bd00df2bab181
Marc Jamot <marcjamot@gmail.com>
2014-11-03 22:09:31 +0100

    Add initial content

---
+++ Source/Slash/Private/Slash.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#include "Slash.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Slash, "Slash" );
+
+DEFINE_LOG_CATEGORY(LogSlash)
+ 
---
+++ Source/Slash/Public/Slash.h
@@ -0,0 +1,11 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __SLASH_H__
+#define __SLASH_H__
+
+#include "EngineMinimal.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogSlash, Log, All);
+
+
+#endif
